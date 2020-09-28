ffb441211d2cb86d25295b959be27a27ef3af19b
akwanmaroso <akwanmaroso@gmail.com>
2020-09-28 13:53:43 +0800

    ref: change sample file

--- auto/data.go
+++ auto/data.go
@@ -3,7 +3,7 @@ package auto
 import "github.com/akwanmaroso/blogos/api/models"
 
 var users = []models.User{
-	models.User{
+	{
 		Nickname: "Jhon Doe",
 		Email:    "jhondoe@email.com",
 		Password: "12345689",
@@ -11,8 +11,8 @@ var users = []models.User{
 }
 
 var posts = []models.Post{
-	models.Post{
-		Title:   "Title",
-		Content: "Content",
+	{
+		Title:   "Lorem Ipsum",
+		Content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus convallis, ante sit amet porta commodo, libero felis suscipit neque, ac aliquet nibh lacus vel odio. Praesent metus elit, maximus sollicitudin aliquet eu, rhoncus sit amet massa. Aliquam rhoncus feugiat ligula in fermentum. Curabitur vehicula viverra augue, non fermentum sem pellentesque vitae. Maecenas venenatis nisl scelerisque, lacinia quam at, scelerisque lectus. Nulla et aliquam purus, quis vehicula turpis. Praesent efficitur sollicitudin enim, pretium lacinia nunc laoreet vitae. Praesent vehicula vitae ipsum quis fringilla. Mauris nisi ante, sagittis quis facilisis ac, bibendum et lacus. Mauris nec est non eros congue pharetra eget at leo. Nulla auctor mauris sit amet turpis porta rutrum. Sed in lorem id risus consequat tempus. Etiam feugiat, sem in porta venenatis, mauris arcu fringilla est, at ultrices nulla lacus in tortor. Donec vel tortor turpis. Etiam ut tortor nec neque venenatis pretium in ut diam. Morbi interdum, diam eget vestibulum tempus, lacus lectus ultricies nisl, at auctor neque justo tempor felis. Duis dictum scelerisque consectetur. Nunc consequat, enim auctor cursus bibendum, felis metus rhoncus purus, sit amet tempor augue libero eget erat. Vestibulum vitae nisl nunc. In posuere nulla dui, in iaculis leo porta ut. Maecenas eu metus non eros malesuada luctus non eu risus. Proin rhoncus urna augue, eu interdum nulla cursus eu. Ut sit amet libero orci. Morbi eget imperdiet nunc. Proin congue metus vel tortor accumsan faucibus. Pellentesque tempor arcu id rutrum mattis. Mauris finibus vel dui a consequat. Proin et rhoncus elit. Duis tincidunt, libero quis sodales mollis, felis felis luctus tortor, non consequat velit justo ac nisi. Aliquam erat volutpat. Proin eleifend, nisi vel venenatis mollis, libero elit pulvinar turpis, et volutpat odio augue vitae arcu. Pellentesque ligula lacus, aliquam et mattis nec, finibus at turpis. Vivamus sit amet euismod leo. In accumsan aliquam ex, eu blandit tortor maximus eget. Nunc porta a nulla nec bibendum. Quisque bibendum volutpat diam ut fringilla. Phasellus semper nulla id dolor feugiat aliquet. Sed finibus auctor sapien, sit amet sollicitudin tellus semper vitae. Fusce tincidunt, justo dapibus dictum cursus, metus est efficitur arcu, et semper enim enim sit amet orci. In rutrum, dui vel interdum tincidunt, justo eros dignissim eros, vitae volutpat diam sapien eget tellus. Donec posuere est eu odio consectetur, vel tincidunt tellus ornare. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pretium, dolor sed vulputate semper, massa mauris eleifend neque, eget volutpat velit justo id neque. Fusce vulputate vehicula hendrerit. Quisque molestie felis vitae leo ultricies, vel mollis urna consectetur. Donec a condimentum dolor, eget hendrerit odio. Maecenas id blandit felis. Suspendisse aliquam ligula in nunc placerat tristique. Morbi pellentesque ex ut ligula imperdiet vehicula. Donec viverra, dolor",
 	},
 }
